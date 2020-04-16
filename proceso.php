919ff8d3428578ca0eccac4a9756301c5248693c
cesarorrego123 <62620327+cesar135@users.noreply.github.com>
2020-04-16 16:57:19 -0600

    Update proceso.php
    realize la maquetacion 2

--- proceso.php
+++ proceso.php
@@ -3,33 +3,41 @@
 	$valor1=$_POST['n1'];
 	$valor2=$_POST['n2'];
 	$opcion=$_POST['opciones'];
+$ total;
 
 	if ($opcion==1) {
-		echo "la suma es: ".($valor1+$valor2);
+$ total=($valor1+$valor2);
 	}
 
 	if ($opcion==2) {
-		echo "la resta es: ".($valor1-$valor2);
+$ total= ($valor1-$valor2);
 	}
 
 	if ($opcion==3) {
-		echo "el producto es: ".($valor1*$valor2);
+		$ total=  ($valor1*$valor2);
 	}
 
 	if ($opcion==4) {
-		echo "la division es: ".($valor1/$valor2);
+$ total=  ($valor1*$valor2);
 	}	
 ?>
-    <html>
-	  <head>
-	  <link rel=" stylesheet"type="text/css"href="Diseño.css"/>
-	  
-	  </head>
-	  <body>
-	   <div align="center" id="mac">
-	   <h1>
-	   Resultado
-	   </h1>
-	   </div>
-	  </body>
-	  </html>
+       <html>
+
+  <head>
+   <link rel="stylesheet" type="text/css" href="Estilos.css" />
+  </head>
+
+  <body>
+   <header>
+          <div align="center" id="TOTAL">
+              <h1> TOTAL </h1>
+          </div>
+   </header>
+
+   <section>
+          <div align="center" id="MOSTRAR">
+              <p><?php echo $total; ?> </p>
+          </div>
+   </section> 
+  </body>
+</html>
